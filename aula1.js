0dac13c4bb91746a39be768201982402da4d0baf
Rafael <rafaelcorreasantos2@gmail.com>
2024-02-02 16:07:06 -0300

    Exercicio Aula 1 (JS)

---
+++ aula1.js
@@ -0,0 +1,33 @@
+// Exercicio 1
+
+let idade = prompt('digite sua idade:')
+let nacionalidade = prompt ('brasileira')
+
+alert( idade >= 16 && nacionalidade === 'brasileira')
+
+
+// Exercicio 2
+
+let salario = prompt('Digite Seu Salario:')
+let scoreDeCredito = prompt('Digite Seu Score:')
+let idade2 = prompt('Digite sua idade:')
+
+alert(salario >= 5000 && scoreDeCredito >= 700 && idade2 >= 18)
+
+//Exercicio 3
+
+let navegador = prompt("Digite o navegador:")
+let sistemaOperacional = prompt("Digite o Sistema operacional:")
+
+
+alert(navegador === 'Chrome' || navegador === 'Firefox' && sistemaOperacional === 'Windows' || sistemaOperacional === 'Mac')
+
+// Exercicio 4
+
+let idade4 = prompt('Digite sua idade:')
+let compraMinima = prompt('Quanto gastou?:')
+let novoCliente = prompt('Novo cliente?:')
+
+alert (idade4 >= 60 && compraMinima >= 100 && novoCliente == 'Não')
+
+
